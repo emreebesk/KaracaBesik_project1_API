8cdcd32accc945fc7d5f429b4bb4ef168911c051
Emre Besik <emreebeskk@gmail.com>
2023-11-16 17:35:20 +0300

    .

--- KaracaBesik_project1/Program.cs
+++ KaracaBesik_project1/Program.cs
@@ -41,7 +41,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 app.UseHttpsRedirection();
 
 app.UseRouting();
